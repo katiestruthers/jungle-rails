70a895ba7ca41129a0a8f9a3293083897e0ccccc
Katherine Struthers <ktstruthers@gmail.com>
2023-11-12 14:56:02 -0600

    write tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('opens the app', () => {})
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
